c2dbc592c80a0afd420481f52345d2c8753e191a
Jesus Romero <jhromero@tekmexico.com>
2019-08-21 12:11:54 -0700

    Added queries playground file with user and todo

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,6 +1,18 @@
 const { mongoose } = require('./../server/db/mongoose');
 const { Todo } = require('./../server/models/todo');
-var id = "5d5c7ecf1ec7c347f42befd2";
-Todo.findById(id).then((todos) => {
-    console.log('Todos', todos);
-});
+const { User } = require('./../server/models/user');
+const { ObjectID } = require('mongodb');
+var idTodo = "5d5c7ecf1ec7c347f42befd2";
+var idUser = "5d5ad0d6d870a42eb05ba545";
+Todo.findById(idTodo).then((todo) => {
+    if (!todo) {
+        return console.log('Id not found');
+    }
+    console.log('Todo by Id', todo);
+}).catch((e) => console.log(e));
+User.findById(idUser).then((user) => {
+    if (!user) {
+        return console.log('Id not found');
+    }
+    console.log('User by Id', user);
+}).catch((e) => console.log(e));
